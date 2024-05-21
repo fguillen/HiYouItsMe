ebe6d8ba09aef6630808d0b37a29cea435b36512
Fernando Guillen <fguillen.mail@gmail.com>
2024-05-21 17:15:02 +0200

    Changed name project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "hello_you_its_me_production"
+  # config.active_job.queue_name_prefix = "hi_you_its_me_production"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
