e6fd858dc86e886eeebff35291e2372b2f6cb355
Fernando Guillen <fguillen.mail@gmail.com>
2024-05-22 22:55:28 +0200

    Animation when new message created

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  after_create_commit { broadcast_append_to "messages" }
+  after_create_commit { broadcast_append_to "messages", locals: { just_created: true } }
 
   scope :by_recent, -> { order(created_at: :desc) }
 end
